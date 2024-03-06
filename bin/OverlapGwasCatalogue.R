337aac28cafabace0723bc0726408f6ffc6f267b
Urmo Võsa <urmo.vosa@gmail.com>
2024-03-06 10:44:14 +0200

    Bugfix

--- bin/OverlapGwasCatalogue.R
+++ bin/OverlapGwasCatalogue.R
@@ -24,7 +24,7 @@ reframe(pheno = pheno, phenotypes = paste(unique(`DISEASE/TRAIT`), collapse = ";
 ontologies = paste(unique(`MAPPED_TRAIT`), collapse = "; ")) %>%
 unique()
 
-novel_variants <- unique(comb_summary2[!comb_summary2$lead_SNP %in% comb$lead_SNP, c(1, 2), with = FALSE])
+novel_variants <- unique(comb[!comb$lead_SNP %in% comb_summary2$lead_SNP, c(1, 2), with = FALSE])
 
 fwrite(novel_variants, "GWAS_catalogue_overlap_novel.txt", sep = "\t")
 fwrite(comb_summary2, "GWAS_catalogue_overlap_summary.txt", sep = "\t")
