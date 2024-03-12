5e293c7ebefb554411db3b64bbf7619350ab9f39
aalksnk <109703482+aalksnk@users.noreply.github.com>
2024-03-12 10:43:17 +0200

    Update OverlapOpenTargets.R

--- bin/OverlapOpenTargets.R
+++ bin/OverlapOpenTargets.R
@@ -10,17 +10,20 @@ proxies <- fread(args[1])
 proxies$proxy_ID <- paste(proxies$proxy_SNP_chr, proxies$proxy_SNP_bp, sep = "_")
 
 opentargets <- open_dataset(args[2]) %>%
-mutate(snp_ID = paste(lead_chrom, lead_pos, sep = "_")) %>%
-filter(tag_pos == lead_pos) %>%
-filter(snp_ID %in% !!proxies$proxy_ID) %>%
-collect()
+  mutate(snp_ID = paste(lead_chrom, lead_pos, sep = "_")) %>%
+  filter(tag_pos == lead_pos) %>%
+  filter(snp_ID %in% !!proxies$proxy_ID) %>%
+  collect()
+
+# Collapse the elements of 'trait_efos' and 'ancestry_initial' into comma-separated strings
+opentargets$trait_efos <- sapply(opentargets$trait_efos, function(x) paste(x, collapse = ", "))
+opentargets$ancestry_initial <- sapply(opentargets$ancestry_initial, function(x) paste(x, collapse = ", "))
+opentargets$ancestry_replication <- sapply(opentargets$ancestry_replication, function(x) paste(x, collapse = ", "))
 
 comb <- merge(proxies, as.data.table(opentargets), by.x = "proxy_ID", by.y = "snp_ID", all.x = TRUE)
 comb <- comb[, -1, with = FALSE]
 
 comb <- comb[order(comb$pheno), ]
-
-comb <- comb[, lapply(.SD, as.character)]
 
 comb_summary <- comb[, c(1, 2, 9:ncol(comb)), with = FALSE]
 
@@ -35,7 +38,7 @@ unique()
 novel_variants <- unique(comb[!comb$lead_SNP %in% comb_summary2$lead_SNP, c(1, 2), with = FALSE])
 
 #print(apply(comb, 2, class)[1:10])
-
+comb <- comb[, lapply(.SD, as.character)]
 
 fwrite(novel_variants, "OpenTargets_overlap_novel.txt", sep = "\t")
 fwrite(comb_summary2, "OpenTargets_overlap_summary.txt", sep = "\t")
